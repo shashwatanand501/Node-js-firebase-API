1457617b7b3c41f11e941b49ecf1425575d41926
Shashwat Anand <53448100+shashwatanand501@users.noreply.github.com>
2024-04-17 02:43:43 +0530

    Personal data deleted

--- Auth/config.js
+++ Auth/config.js
@@ -4,13 +4,7 @@ require('firebase/firestore');
 require('firebase/storage'); // Import Firebase Storage module
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBPfDJsa-gVHxpcG3sf-Hbeqw3xGjAYVHE",
-    authDomain: "node-js-api-d7072.firebaseapp.com",
-    projectId: "node-js-api-d7072",
-    storageBucket: "node-js-api-d7072.appspot.com",
-    messagingSenderId: "894706229754",
-    appId: "1:894706229754:web:8b37ae7fde35aa6b1d8443",
-    measurementId: "G-ETH4YNWHNK"
+    
 };
 
 firebase.initializeApp(firebaseConfig);
